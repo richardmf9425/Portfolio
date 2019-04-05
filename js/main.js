a2be00733f2176eb86d9e296288fb48a8b476c56
Richard Mena <richardmf9425@yahoo.com>
2019-04-05 01:57:30 -0400

    Add navbar effects and hamburger effect

--- js/main.js
+++ js/main.js
@@ -5,39 +5,51 @@ const menu = document.querySelector('.menu');
 const menuNav = document.querySelector('.menu-nav');
 const menuBranding = document.querySelector('.menu-branding');
 
+
 const navItems = document.querySelectorAll('.nav-item');
 
-//Set initial state of menu
+// Set initial state of menu
 let showMenu = false;
 
 menuBtn.addEventListener('click', toggleMenu);
 
 function toggleMenu() {
-    if (!showMenu) {
-        menuBtn.classList.add('close');
-        menu.classList.add('show');
-        menuNav.classList.add('show');
-        menuBranding.classList.add('show');
+  if (!showMenu) {
+    menuBtn.classList.add('close');
+    menu.classList.add('show');
+    menuNav.classList.add('show');
+    menuBtn.classList.remove('menu-btn-back');
 
-        navItems.forEach(item => item.classList.add('show'));
-        // set menu state
-        showMenu = true;
-    } else {
-        menuBtn.classList.remove('close');
-        menu.classList.remove('show');
-        menuNav.classList.remove('show');
-        menuBranding.classList.remove('show');
+    // menuBranding.classList.add('show');
 
-        navItems.forEach(item => item.classList.remove('show'));
-        // set menu state
-        showMenu = false;
+    navItems.forEach(item => item.classList.add('show'));
+    // set menu state
+    showMenu = true;
+  } else {
+    menuBtn.classList.remove('close');
+    menu.classList.remove('show');
+    menuNav.classList.remove('show');
+    // menuBranding.classList.remove('show');
+    menuBtn.classList.add('menu-btn-back');
 
-    }
+    navItems.forEach(item => item.classList.remove('show'));
+    // set menu state
+    showMenu = false;
+  }
 }
 
 // remove logo icon when scroll
 window.onscroll = () => {
-    const logo = document.querySelector('.logo');
-    if (this.scrollY <= 10) logo.classList.remove('scroll');
-    else logo.classList.add('scroll');
+  const logo = document.querySelector('.logo');
+  const navbar = document.querySelector('.menu-nav');
+  const topButton = document.querySelector('.top-button');
+  if (this.scrollY <= 10) {
+    logo.classList.remove('scroll');
+    navbar.classList.remove('scroll');
+    topButton.classList.add('scroll');
+  } else {
+    logo.classList.add('scroll');
+    navbar.classList.add('scroll');
+    topButton.classList.remove('scroll');
+  }
 };
